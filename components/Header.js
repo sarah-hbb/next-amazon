83ca0c226a98e9907d8532a67a825b26addfef0f
sarah-hbb <sarahabibi.89@gmail.com>
2022-11-25 15:21:57 +0100

    change in header component

--- components/Header.js
+++ components/Header.js
@@ -65,7 +65,7 @@ const Header = () => {
         </p>
         <p className="link">Prime Video</p>
         <p className="link">Amazon Business</p>
-        <p className="link">Today's Deals</p>
+        <p className="link">Todays Deals</p>
         <p className="link hidden lg:inline-flex">Electronics</p>
         <p className="link hidden lg:inline-flex">Food & Grocery</p>
         <p className="link hidden lg:inline-flex">Prime</p>
